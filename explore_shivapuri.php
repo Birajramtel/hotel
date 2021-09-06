84d2011003c730aff0ef9ea7d5557eafebda57ed
Birajramtel <biraj.ramtel007@gmail.com>
2021-09-06 12:03:29 +0545

    Changes made from office

---
+++ explore_shivapuri.php
@@ -0,0 +1,321 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Naava Hotel</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.css" integrity="sha512-UTNP5BXLIptsaj5WdKFrkFov94lDx+eBvbKyoe1YAfjeRPC+gT5kyZ10kOHCfNZqEui1sxmqvodNUx3KbuYI/A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="js/owl.carousel.js">
+    <link rel="stylesheet" href="js/owl.carousel.min.js">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+</head>
+
+<body>
+    <!-- Header Opened -->
+    <!-- NavBar started -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="img/Naava PP.png" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="mx-auto"></div>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="about.php">About Naava</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="explore_shivapuri.php">Explore Shivapuri</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="accommodation.php">Accommodation</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="eat_and_drink.php">Eat and Drink</a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link active" href="#">Packages</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Highlights</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="contact.php">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Banner Image  -->
+    <div class="about_cover">
+        <div class="about_image">
+            <img src="img/Hotel_front_about.png" class="d-block w-100" alt=" Hotel Front">
+        </div>
+        <div class="container about_breadcrumb d-none d-md-block">
+            <h2 class="header_2 text-white"><strong><b>Explore Shivapuri</b></strong> </h2>
+            <ul class="breadcrumb pl-1">
+                <li><a href="index.php">Home </a></li>
+                <li><a href="accommodation.php">Explore Shivapuri</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- Header Closed -->
+
+    <!-- Explore Shivapuri Content Session Opened -->
+    <div class="container mt-5 ">
+        <h2 class="Header_2"><strong><b>EXPLORE SHIVAPURI</b></strong></h2>
+        <p class="para_align ml-4">
+            At an elevation of 8963 ft on the Northern side of Kathmandu, Shivapuri is the second largest hill 
+            surrounding the valley. Only an hour away from Kathmandu, it is a popular destination for hikers, 
+            mountain bikers, researchers, bird watchers and pilgrims.
+        </p>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h6 class="Header_6"> <strong><b> Natural Beauty</b> </strong> </h6>
+                <p class="para_align">
+                Shivapuri National Park serves as the ideal destination for nature lovers, without having to go very 
+                far from the city. On your way to the top, you can observe luxurious bamboo and rhododendron forests 
+                along with snow capped mountains. You can enjoy the view of Ganesh Himal, Langtang Himal and Jugal 
+                himals in the north and Manaslu Himal in the south. One can observe several forest types on the way 
+                to the top such as pine, Utis, oak etc. Similarly, the hill hosts rare orchids and exotic bird species. 
+                Small waterfalls and rivulets add to the beauty of the hill. 
+                </p>
+            </div>
+            <div class="col-md-6 pb-5">
+                <img src="img/Bishnudwaar.png" alt="bishnudwaar" class="img-fluid pl-5 w-100">
+            </div>
+            <div class="col-md-6 pb-5">
+                <img src="img/Teen Chuli.png" alt="Teen Chuli" class="img-fluid">
+            </div>
+            <div class="col-md-6">
+                <h6 class="Header_6"> <strong><b>Flora and Fauna</b> </strong> </h6>
+                <p class="para_align">
+                    Shivapuri Nagarjun National Park is a true paradise for flora and fauna. 
+                    The park has recorded 1250 species of vascular plants and 129 species of mushrooms. 
+                    Chinkapin, Pine, and Oak-Rhododendron are the dominant vegetation. One can observe four 
+                    major forest types viz
+                </p> 
+                <ul style="list-style-type: circle;">
+                    <li>Lower mixed hardwood forests</li>
+                    <li>Chirpine forests</li>
+                    <li>Oak forests</li>
+                    <li>Upper mixed hardwood forests</li>
+                </ul>
+            </div>
+            <div class="col-md-6">
+                <h6 class="Header_6"><strong><b>Watershed Value</b></strong></h6>
+                <p class="para_align">
+                Shivapuri is the prime source of drinking water for Kathmandu valley. Small rivers and streams 
+                discharge about 30 million liters of water per day. The major sources include Nagmati, Bagmati, 
+                Syalmati, Bishnumati and their tributaries. Drinking water, irrigation, holy bathing are the major 
+                uses of the rivers.
+            </div>
+            <div class="col-md-6 pb-5">
+                <img src="img/Bishnudwaar.png" alt="bishnudwaar" class="img-fluid pl-5 w-100">
+            </div>
+            <div class="col-md-6 pb-5">
+                <img src="img/Teen Chuli.png" alt="Teen Chuli" class="img-fluid">
+            </div>
+            <div class="col-md-6">
+                <h6 class="Header_6"> <strong><b>Adventure </b> </strong> </h6>
+                <p class="para_align">
+                    Adventure seekers can go for mountain biking, hiking and jeep safari along the area. After receiving 
+                    the permit from the park, one can hike up to Nagi Gumba through a rugged trail alongside cherry and 
+                    pine forests. This monastery is about an hour and a half away from the gateway. From there, you can 
+                    hike upto the peak in about three hours along a steep route uphill. 
+                </p> 
+            </div>
+            <div class="col-md-6">
+                <h6 class="Header_6"><strong><b>Research and study</b></strong></h6>
+                <p class="para_align">
+                Shivapuri National Park has been the destination for researchers and nature enthusiasts. Several studies 
+                regarding flora and fauna, climate and topography are conducted here. 
+                Clouded leopard, pangolin, Assamese monkey and leopard cat are the protected mammals found in SNNP. 
+                Common mammals include common leopard, Himalayan black bear, Himalayan goral, Naemorhedus goral, jungle 
+                cat, Himalayan, wild boar, barking deer, sambar deer, rhesus monkey, civets, porcupine and yellow 
+                throated marten. The park has 311 species of birds including 117 migratory birds. Common species of bird 
+                species are white backed vulture, Himalayan griffon, black vulture, beard vulture (Nepali &Suwal, 2007),
+                 dark kite, hen harrier, goshawk, sparrow hawk, sikhra, common buzzard, Asian black eagle, steppe eagle 
+                 (BPP, 1995), magpies, kalij pheasant, leafbirds, flycatcher, bushchat, babblers, cuckoos, bulbul, swift, 
+                 warblers. Importantly, the spiny babler, and the wren babler are the endemic birds found in Shivapuri 
+                 Nagarjun national park. 
+
+            </div>
+            <div class="col-md-6 pb-5">
+                <img src="img/Bishnudwaar.png" alt="bishnudwaar" class="img-fluid pl-5 w-100">
+            </div>
+            <div class="col-md-6 pb-5">
+                <img src="img/Teen Chuli.png" alt="Teen Chuli" class="img-fluid">
+            </div>
+            <div class="col-md-6">
+                <h6 class="Header_6"> <strong><b>Pilgrimage</b> </strong> </h6>
+                <p class="para_align">
+                    Shivapuri is home to several religious and pilgrimage sites for the Hindus and Buddhists. 
+                    Buddhanilkantha, the prestigious Hindu temple with the magnificent statue of the sleeping 
+                    god Vishnu lies on the foot of the Shivapuri hill. Similarly, at an altitude of 1650 m above 
+                    the Budhanilkantha temple lies the famous Buddhist monastery ‘Naagi Gumba’, the only monastery 
+                    for Nuns where about 200 of them practice Buddhism. Shivapuri also houses Baagdwar and Bishnudwar, 
+                    the origin points of sacred rivers Bagmati and Bishnumati respectively. Every year on Baisakh 1, 
+                    the Nepalese New year’s day, a religious fair is held at Baagdwar and hindu devotees flock to the 
+                    site to take a holy bath early in the morning, with the belief that doing so will wash away their 
+                    sins. Both Baagdwar and Bishnudwar hold great religious significance.
+                </p> 
+            </div>
+        </div>
+    </div>
+    <!-- Explore Shivapuri Content Session Closed -->
+
+
+    <!-- Photo Session Opened -->
+    <div class="container">
+        <h6 class="Header_6"><strong>PHOTOS</strong></h6>
+        <h2 class="Header_2 col-lg-8 col-sm-12"><strong><b>See our latest photos From restaurant and hotel</b></strong></h2>
+    </div>
+    <div class="owl-carousel owl-theme">
+        <div class="item">
+            <img src="img/Table_final.png" class="float-right" alt="Table Final">
+        </div>
+        <div class="item">
+            <img src="img/Bishnudwaar_final.png" class="float-right" alt="Bishnudwaar Hiking">
+        </div>
+        <div class="item">
+            <img src="img/Bed_final.png" class="float-right" alt="Hotel Bed">
+        </div>
+        <div class="item">
+            <img src="img/Seminar_final.png" class="float-right" alt="Seminar Hall">
+        </div>
+    </div>
+    <!-- Photo Session Closed -->
+    </section>
+    <!-- Footer Session Opened -->
+    <footer class="footer-section">
+        <div class="container">
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
+                    <!-- <img src="img/Naava PP.png" alt="Logo"> -->
+                    <h2 class="text-white">Naava Resort & Retreat</h2>
+                    <p class="text-white">Our hotel is an image of quality and profesionalism combined with hard work and precision.
+                        You can expect only the best services from our staff.
+                    </p>
+                    <div class="footer-social-icon mt-2">
+                        <a href="#"><i class="fab fa-facebook-f facebook-bg"></i></a>
+                        <a href="#"><i class="fab fa-twitter twitter-bg"></i></a>
+                        <a href="#"><i class="fab fa-google-plus-g google-bg"></i></a>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
+                    <h2 class="text-white">Useful Links</h2>
+                    <ul style="list-style-type:none;">
+                        <li><a class="footer_link" href="#">Blog</a></li>
+                        <li><a class="footer_link" href="#">Rooms</a></li>
+                        <li><a class="footer_link" href="#">Gift Card</a></li>
+                        <li><a class="footer_link" href="#">Testimonials</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
+                    <h2 class="text-white">Privacy</h2>
+                    <ul style="list-style-type: none;">
+                        <li><a class="footer_link" href="#">Career</a></li>
+                        <li><a class="footer_link" href="#">About Us</a></li>
+                        <li><a class="footer_link" href="#">Contact us</a></li>
+                        <li><a class="footer_link" href="#">Services</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
+                    <h2 class="text-white">Contact</h2>
+                    <div class="footer-social-icon">
+                        <div class="single-cta">
+                            <i class="far fa-envelope-open"></i>&nbsp;<span class="text-white">example@naava.com</span>
+                        </div>
+                        <div class="single-cta">
+                            <i class="fas fa-phone-alt"></i>&nbsp;<span class="text-white">+0 320 422 4254</span>
+                        </div>
+                        <div class="single-cta">
+                            <i class="fas fa-map-marker-alt"></i>&nbsp;<span class="text-white">Main Str Nepal</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col-6">
+                    <p class="text-white float-start">Copyright &copy; 2018, All Right Reserved SewaTech</p>
+                </div>
+                <div class="col-6">
+                    <a class="footer_link float-end" href="#">https://naavaresort.com.np</a>
+                </div>
+            </div>
+        </div>
+        </div>
+    </footer>
+    <!-- Footer Session Closed -->
+    <script src="js/owl.carousel.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+        -->
+
+    <!--  jQuery 1.7+  -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="jquery-1.9.1.min.js"></script>
+
+    <!-- Include js plugin -->
+    <script src="assets/owl-carousel/owl.carousel.js"></script>
+    <script>
+        var nav = document.querySelector("nav");
+
+        window.addEventListener("scroll", function() {
+            if (window.pageYOffset > 100) {
+                nav.classList.add("bg-dark", "shadow");
+            } else {
+                nav.classList.remove("bg-dark", "shadow");
+            }
+        });
+    </script>
+
+    <script>
+        $(' .owl-carousel').owlCarousel({
+            loop: true,
+            margin: 0,
+            nav: false,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 3
+                },
+                1000: {
+                    items: 5
+                }
+            }
+        })
+    </script>
+
+</body>
+
+</html>
