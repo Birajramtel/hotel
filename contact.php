ddcd10ce99cb469a64618c851170f408dababfdf
Birajramtel <biraj.ramtel007@gmail.com>
2021-09-06 16:58:11 +0545

    Changes made in Contact us page

--- contact.php
+++ contact.php
@@ -106,16 +106,17 @@
             </div>
         </form>
     </div>
-    <div class="container pb-2">
+    
+    <div class="container footer-social-icon pb-2">
         <div class="row mt-5 mb-2">
-            <div class="col-md-4 ">
-                <i class="far fa-envelope-open"></i>&nbsp;<span class="">example@naava.com</span>
+            <div class="col-md-4 col-sm-4 single-cta">
+                <i class="far fa-envelope-open"></i>&nbsp;<span><strong>example@naava.com</strong></span>
             </div>
-            <div class="col-md-4 ">
-                <i class="fas fa-phone-alt"></i>&nbsp;<span class="">+0 320 422 4254</span>
+            <div class="col-md-4 col-sm-4 single-cta">
+                <i class="fas fa-phone-alt"></i>&nbsp;<span><strong>+0 320 422 4254</strong></span>
             </div>
-            <div class="col-md-4 ">
-                <i class="fas fa-map-marker-alt"></i>&nbsp;<span class="">Main Str Nepal</span>
+            <div class="col-md-4 col-sm-4 single-cta">
+                <i class="fas fa-map-marker-alt"></i>&nbsp;<span><strong>Main Str Nepal</strong></span>
             </div>
         </div>
     </div>
@@ -155,7 +156,7 @@
                         You can expect only the best services from our staff.
                     </p>
                     <div class="footer-social-icon mt-2">
-                        <a href="#"><i class="fab fa-facebook-f facebook-bg"></i></a>
+                        <a href="https://www.facebook.com/Naava-Resort-Retreat-100547551927011/"><i class="fab fa-facebook-f facebook-bg"></i></a>
                         <a href="#"><i class="fab fa-twitter twitter-bg"></i></a>
                         <a href="#"><i class="fab fa-google-plus-g google-bg"></i></a>
                     </div>
@@ -164,7 +165,7 @@
                     <h2 class="text-white">Useful Links</h2>
                     <ul style="list-style-type:none;">
                         <li><a class="footer_link" href="#">Blog</a></li>
-                        <li><a class="footer_link" href="#">Rooms</a></li>
+                        <li><a class="footer_link" href="package.php">Rooms</a></li>
                         <li><a class="footer_link" href="#">Gift Card</a></li>
                         <li><a class="footer_link" href="#">Testimonials</a></li>
                     </ul>
@@ -173,8 +174,8 @@
                     <h2 class="text-white">Privacy</h2>
                     <ul style="list-style-type: none;">
                         <li><a class="footer_link" href="#">Career</a></li>
-                        <li><a class="footer_link" href="#">About Us</a></li>
-                        <li><a class="footer_link" href="#">Contact us</a></li>
+                        <li><a class="footer_link" href="about.php">About Us</a></li>
+                        <li><a class="footer_link" href="contact.php">Contact us</a></li>
                         <li><a class="footer_link" href="#">Services</a></li>
                     </ul>
                 </div>
