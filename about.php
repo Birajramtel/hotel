fdce789399a527042a857909cfd9d6f09abe55ff
Birajramtel <biraj.ramtel007@gmail.com>
2021-09-06 12:00:26 +0545

    Changes made in About us Page

--- about.php
+++ about.php
@@ -37,7 +37,7 @@
                         <a class="nav-link active" href="about.php">About Naava</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="#">Explore Shivapuri</a>
+                        <a class="nav-link active" href="explore_shivapuri.php">Explore Shivapuri</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link active" href="accommodation.php">Accommodation</a>
