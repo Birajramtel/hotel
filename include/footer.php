df7f36e18f80e3dd2b8f8382955901bf47fbdfba
Birajramtel <biraj.ramtel007@gmail.com>
2021-09-08 14:50:02 +0545

    Footer content changed

--- include/footer.php
+++ include/footer.php
@@ -1,25 +1,59 @@
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-        -->
-    <script>
-        var nav = document.querySelector("nav");
-
-        window.addEventListener("scroll", function() {
-            if (window.pageYOffset > 100) {
-                nav.classList.add("bg-dark", "shadow");
-            } else {
-                nav.classList.remove("bg-dark", "shadow");
-            }
-        });
-    </script>
-
-    </body>
-
-    </html>
+<footer class="footer-section">
+        <div class="container">
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
+                    <!-- <img src="img/Naava PP.png" alt="Logo"> -->
+                    <h2 class="text-white">Naava Resort & Retreat</h2>
+                    <p class="text-white">Our hotel is an image of quality and profesionalism combined with hard work and precision.
+                        You can expect only the best services from our staff.
+                    </p>
+                    <div class="footer-social-icon mt-2">
+                        <a href="https://www.facebook.com/Naava-Resort-Retreat-100547551927011/"><i class="fab fa-facebook-f facebook-bg"></i></a>
+                        <a href="#"><i class="fab fa-twitter twitter-bg"></i></a>
+                        <a href="#"><i class="fab fa-google-plus-g google-bg"></i></a>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
+                    <h2 class="text-white">Useful Links</h2>
+                    <ul style="list-style-type:none;">
+                        <li><a class="footer_link" href="#">Blog</a></li>
+                        <li><a class="footer_link" href="package.php">Rooms</a></li>
+                        <li><a class="footer_link" href="#">Gift Card</a></li>
+                        <li><a class="footer_link" href="#">Testimonials</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
+                    <h2 class="text-white">Privacy</h2>
+                    <ul style="list-style-type: none;">
+                        <li><a class="footer_link" href="#">Career</a></li>
+                        <li><a class="footer_link" href="about.php">About Us</a></li>
+                        <li><a class="footer_link" href="contact.php">Contact us</a></li>
+                        <li><a class="footer_link" href="#">Services</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xm-12 mt-3">
+                    <h2 class="text-white">Contact</h2>
+                    <div class="footer-social-icon">
+                        <div class="single-cta">
+                            <i class="far fa-envelope-open"></i>&nbsp;<span class="text-white">example@naava.com</span>
+                        </div>
+                        <div class="single-cta">
+                            <i class="fas fa-phone-alt"></i>&nbsp;<span class="text-white">+0 320 422 4254</span>
+                        </div>
+                        <div class="single-cta">
+                            <i class="fas fa-map-marker-alt"></i>&nbsp;<span class="text-white">Main Str Nepal</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col-6">
+                    <p class="text-white float-start">Copyright &copy; 2018, All Right Reserved SewaTech</p>
+                </div>
+                <div class="col-6">
+                    <a class="footer_link float-end" href="#">https://naavaresort.com.np</a>
+                </div>
+            </div>
+        </div>
+        </div>
+    </footer>
