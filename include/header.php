1d78813b139c1ed18bd0cfce9dda08263580c898
Birajramtel <biraj.ramtel007@gmail.com>
2021-09-08 21:19:48 +0545

    Modified Included Header from Home

--- include/header.php
+++ include/header.php
@@ -1,42 +1,42 @@
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3" id="bg_nav">
-        <div class="container">
-            <a class="navbar-brand" href="index.php">
-                <img src="img/Naava PP.png" alt="">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <div class="mx-auto"></div>
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="index.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="about.php">About Naava</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="explore_shivapuri.php">Explore Shivapuri</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="accommodation.php">Accommodation</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="eat_and_drink.php">Eat and Drink</a>
-                    </li>
-                    <li class="nav-item ">
-                        <a class="nav-link text-white" href="package.php">Packages</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="highlight.php">Highlights</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="gallery.php">Gallery</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="contact.php">Contact Us</a>
-                    </li>
-                </ul>
-            </div>
+    <div class="container">
+        <a class="navbar-brand" href="index.php">
+            <img src="img/Naava PP.png" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="mx-auto"></div>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="about.php">About Naava</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="explore_shivapuri.php">Explore Shivapuri</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="accommodation.php">Accommodation</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="eat_and_drink.php">Eat and Drink</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link text-white" href="package.php">Packages</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="highlight.php">Highlights</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="gallery.php">Gallery</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="contact.php">Contact Us</a>
+                </li>
+            </ul>
         </div>
-    </nav>
+    </div>
+</nav>
